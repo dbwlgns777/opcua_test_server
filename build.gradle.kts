6ecde87fa9329f240389a1134dead3e9a5f0c2ec
dbwlgns777 <52645533+dbwlgns777@users.noreply.github.com>
2026-02-19 18:01:43 +0900

    Change temp dummy node type from UInt16 to Int16

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("application")
 }
 
 group = "org.example"
@@ -10,9 +11,23 @@ repositories {
 }
 
 dependencies {
+    // Milo 0.6.x 계열에서 안정적으로 사용되는 조합
+    implementation("org.eclipse.milo:sdk-server:0.6.14")
+    implementation("org.eclipse.milo:stack-server:0.6.14")
+
+    implementation("org.slf4j:slf4j-simple:2.0.12")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+application {
+    mainClass.set("org.example.Main")
+}
+
+tasks.withType<JavaCompile> {
+    options.release.set(17)
 }
 
 tasks.test {
